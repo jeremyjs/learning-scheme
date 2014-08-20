a694e3f6389ea40ae373a5d6e405e4c76721045c
Jeremy Meyer <jjman505@gmail.com>
2014-08-20 11:51:15 -0400

    cleaned up style and added new functions

--- conditionals.scm
+++ conditionals.scm
@@ -1,20 +1,28 @@
 (define (abs x)
   (if (< x 0)
-    (- x)
-    x
-  )
-)
+    (- x) x))
 
 (define (recip x)
   (if (= x 0)
     #f
-    (/ 1 x)
-  )
-)
+    (/ 1 x)))
 
 (define (int->ascii c)
   (if (or (< c 33) (> c 126))
     #f
-    (integer->char c)
-  )
-)
+    (integer->char c)))
+
+(define (f x y z)
+  (and (> x 0) (> y 0) (> z 0) (* x y z)))
+
+(define (f2 x y z)
+  (if (and (> x 0) (> y 0) (> z 0))
+    (* x y z) #f))
+
+(define (g x y z)
+  (and (or (< x 0) (< y 0) (< z 0)) (* x y z)))
+
+(define (g2 x y z)
+  (if (or (< x 0) (< y 0) (< z 0))
+    (* x y z) #f))
+
